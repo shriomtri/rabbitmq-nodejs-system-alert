f9558903c2740ec69c0f26731b9b966862c06325
shriom <shriomtripathi33@gmail.com>
2021-11-06 18:50:47 +0530

    Update routing for critical-limit routing key

--- src/modules/book/routes.js
+++ src/modules/book/routes.js
@@ -17,4 +17,11 @@ router.get('/limit', (req, res, next) => {
   MQ.channel.publish(alert.exchange.exchange, alert.queue_rate_limit.routing_key , Buffer.from(JSON.stringify('Got rate limit request')));
 })
 
+router.get('/critical-limit', (req, res, next) => {
+  res.status(200);
+  res.end();
+  console.log('critical & limit message sending')
+  MQ.channel.publish(alert.exchange.exchange, 'critical.rate_limit', Buffer.from(JSON.stringify('Got critical & limit request')))
+})
+
 module.exports = router;
